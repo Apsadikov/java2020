31339777b6618c832e13496b8733e844f31f7acd
Danil Apsadikov <DonPonchik18@gmail.com>
2019-09-26 18:01:02 +0300

    HW03 added

---
+++ HW03/src/LoginServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("login.jsp");
+        view.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (login.equals("admin") && password.equals("1234")) {
+            resp.sendRedirect("../ok.jsp");
+        } else {
+            resp.sendRedirect("../login");
+        }
+    }
+}
